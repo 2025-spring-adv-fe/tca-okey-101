f38e9f216d8f65a9e45a1fad9e39a3cd08190a95
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-02-06 13:01:13 -0600

    Learned about padding margins etc. on DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ function App() {
   return (
     <div>
       <h1
-        className='text-2xl font-bold'
+        className='text-2xl font-bold bg-base-300 p-4 text-secondary'
       >
         TCA Okey 101
       </h1>
       <button
-        className='btn btn-success btn-soft btn-xl'
+        className='btn btn-success btn-soft btn-xl p-4 my-4'
       >
         Play Okey 101
       </button>
+      <h2
+        className='mt-3 text-xl font-semi-bold'
+      >
+        Leaderboard...
+      </h2>
     </div>
   )
 }
