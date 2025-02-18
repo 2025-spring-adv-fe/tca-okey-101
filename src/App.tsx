a0b72d3523410eb038c436ef3a3606baa54eab64
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-02-18 13:35:41 -0600

    Got a home component func working with react router
    Next, lets copy it 2 times for our other screens...

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
 import './App.css'
+import {
+  HashRouter
+  , Routes
+  , Route
+} from 'react-router';
 
-interface AppProps {
-  timestamp: string;
-  magicNumber: number;
+const Home = () => {
+
+  return (
+    <>
+      <h3 className='text-2x1 font-bold'>
+        Home
+      </h3>
+      <button className="btn btn-active btn-success btn-lg mt-4">
+        Play Okey 101
+      </button>
+    </>
+  );
 }
-const App: React.FC<AppProps> = (
-  {
-    timestamp
-    , magicNumber
-  }
-) => {
+
+const App = () => {
 
   console.log(
     "App Component Func Called"
-    , timestamp
-    , magicNumber
   );
   return (
-    <div>
-      <h1
-        className='text-2xl font-bold'
-      >
-        TCA Okey 101
-      </h1>
-      <p>
-        { timestamp} - {magicNumber}
-      </p>
-      <button
-        className='btn btn-success btn-active btn-xl'
-      >
-        Play Okey 101
-      </button>
+    <div className='p-4'>
+      <HashRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Home />
+            }
+          />
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
