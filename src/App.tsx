e8281ef375b914f3afc4e28a898ec82e30ddbb73
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-02-18 13:40:09 -0600

    Moved Home to its own file...
    Now, we need to copy the file two times...

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,9 @@ import {
   , Routes
   , Route
 } from 'react-router';
+import { Home } from './Home';
 
-const Home = () => {
 
-  return (
-    <>
-      <h3 className='text-2x1 font-bold'>
-        Home
-      </h3>
-      <button className="btn btn-active btn-success btn-lg mt-4">
-        Play Okey 101
-      </button>
-    </>
-  );
-}
 
 const App = () => {
 
---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+
+    return (
+      <>
+        <h3 className='text-2x1 font-bold'>
+          Home
+        </h3>
+        <button className="btn btn-active btn-success btn-lg mt-4">
+          Play Okey 101
+        </button>
+      </>
+    );
+  };
