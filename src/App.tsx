d43048d7e12396a9f693f83ef183fe6b724e8046
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-02-18 13:10:33 -0600

    Looking at React basics and coding little bit...

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css'
 
-function App() {
+interface AppProps {
+  timestamp: string;
+  magicNumber: number;
+}
+const App: React.FC<AppProps> = (
+  {
+    timestamp
+    , magicNumber
+  }
+) => {
 
+  console.log(
+    "App Component Func Called"
+    , timestamp
+    , magicNumber
+  );
   return (
     <div>
       <h1
@@ -9,8 +23,11 @@ function App() {
       >
         TCA Okey 101
       </h1>
+      <p>
+        { timestamp} - {magicNumber}
+      </p>
       <button
-        className='btn btn-success btn-soft btn-xl'
+        className='btn btn-success btn-active btn-xl'
       >
         Play Okey 101
       </button>
