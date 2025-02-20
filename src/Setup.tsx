eb26f97b025be35fff1d03af3b0bf50ec06aa5bc
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-02-20 15:20:14 -0600

    Navigate from Setup to Play component via button used react-touter...

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router";
+
 export const Setup = () => {
+
+  const foobarcat = useNavigate(); //Just for fun :)
 
     return (
       <>
         <h3 className='text-2x1 font-bold'>
           Setup
         </h3>
-        <button className="btn btn-active btn-success btn-lg mt-4">
+        <button 
+          className="btn btn-active btn-success btn-lg mt-4"
+          onClick={
+            () => foobarcat('/Play')
+          }  
+        >
           Start Playing
         </button>
       </>
