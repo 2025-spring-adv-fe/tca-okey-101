70a23e50510ceb158450d9278db8f1239bd87ec6
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-02-25 13:03:56 -0600

    changed const to let to see the numbers on console, still not working as I wanted...

--- src/Play.tsx
+++ src/Play.tsx
@@ -4,14 +4,24 @@ export const Play = () => {
 
   const nav = useNavigate();
 
+  let turnNumber = 6;
+
     return (
       <>
         <h3 className='text-2x1 font-bold'>
           Play
         </h3>
         <h4 className="text-lg font-semibold">
-          Turn #1
-          <button className="btn btn-xs btn-outline btn-light ml-4">
+          Turn #{turnNumber}
+          <button 
+            className="btn btn-xs btn-outline btn-light ml-4"
+            onClick={
+              () => { 
+                turnNumber = turnNumber + 1;
+                console.log(turnNumber);
+              }
+            }
+          >
             +
           </button>
         </h4>
