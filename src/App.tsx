a84989b3069115b41fb6af72d860f650b186bc29
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-03-13 13:03:23 -0500

    Updated GameResult interface and fixed various TS errors ! ! !

--- src/App.tsx
+++ src/App.tsx
@@ -18,42 +18,8 @@ const dummyGameResults: GameResult[] = [
           , "Harry"
           , "Ron"
       ]
-  }
-  , {
-      winner: "Ron"
-      , players: [
-          "Hermione"
-          , "Ron"
-      ]
-  }
-  , {
-      winner: "Larry"
-      , players: [
-          "Larry"
-          , "Curly"
-          , "Moe"
-      ]
-  }
-  , {
-      winner: "Harry"
-      , players: [
-          "Curly"
-          , "Harry"
-      ]
-  }
-  , {
-      winner: "Ron"
-      , players: [
-          "Ron"
-          , "Voldemort"
-      ]
-  }
-  , {
-      winner: "Voldemort"
-      , players: [
-          "Ron"
-          , "Voldemort"
-      ]
+      , start: "2025-03-05T18:40:27.576Z"
+      , end: "2025-03-05T18:45:42.576Z"
   }
 ];
 
