2a47b89f4795007cddb138c70bd794258ef9b4eb
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-03-29 22:23:13 -0500

    Better UI/UX
    && Added key prop to get rid of react console warning...

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -13,6 +13,10 @@ export const Setup: React.FC<SetupProps> = ({
   , setCurrentPlayers
 }) => {
 
+  //
+  // React hooks, includes, state, effects, and others...
+  //
+
   useEffect(
     () => setTitle("Setup")
     , []
@@ -28,10 +32,20 @@ export const Setup: React.FC<SetupProps> = ({
     )
   );
 
+  //
+  // Other code, for example, derived state and other calcs...
+  //
+  const numberOfChosenPlayers = availablePlayers.filter(x => x.checked).length;
+  const fourPlayersChosen = numberOfChosenPlayers === 4;
+
+  //
+  // Return the JSX...
+  //
+
     return (
       <>
         <button 
-          className="btn btn-active btn-success btn-lg mt-4"
+          className="btn btn-active btn-success btn-lg mt-4 w-full lg:w-64"
           onClick={
             () => {
               setCurrentPlayers(
@@ -48,8 +62,13 @@ export const Setup: React.FC<SetupProps> = ({
               nav('/play');
             }
           }  
+          disabled={!fourPlayersChosen}
         >
-          Start Playing
+          {
+            fourPlayersChosen
+              ? "Start Playing"
+              : "Choose 4 Players"
+          }
         </button>
         <div 
           className="mt-4"
@@ -58,6 +77,7 @@ export const Setup: React.FC<SetupProps> = ({
             availablePlayers.map(
               x => (
                   <label
+                    key={x.name}
                     className="block mt-2"
                   >
                     <input 
